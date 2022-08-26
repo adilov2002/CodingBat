60db24b95865459d7b7b1e4627bd7f261c16c561
adilov2002 <joni.2002.08@gmail.com>
2022-08-26 23:16:40 +0600

    started Logic2 topic

---
+++ src/Logic2.java
@@ -0,0 +1,24 @@
+public class Logic2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean makeBricks(int small, int big, int goal) {
+        if (big * 5 == goal || small == goal || big == goal || (big <= goal && small >= goal)) {
+            return true;
+        }
+        int current = big * 5;
+        if (current < goal) {
+            if (current + small >= goal) {
+                return true;
+            }
+        }
+        if (current > goal) {
+            if (goal % 5 <= small) {
+                return true;
+            }
+            return current % goal == 5;
+        }
+        return false;
+    }
+}
