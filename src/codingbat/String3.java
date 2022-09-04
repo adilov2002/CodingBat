823fb98610e5b47a6ca1475107588f1cc0a16156
adilov2002 <joni.2002.08@gmail.com>
2022-09-04 23:01:33 +0600

    codingbat: finished String3 2nd task

--- src/codingbat/String3.java
+++ src/codingbat/String3.java
@@ -3,7 +3,42 @@ package codingbat;
 public class String3 {
 
     public static void main(String[] args) {
-        System.out.println(countYZ("yak zak"));
+        System.out.println(withoutString("Hellollo there", "llo"));
+    }
+
+    public static String withoutString(String base, String remove) {
+        StringBuilder res = new StringBuilder();
+        for (int j = 0; j < base.length(); j++) {
+            if (remove.length() + j <= base.length() && base.substring(j, remove.length() + j).equalsIgnoreCase(remove)) {
+                j = j + remove.length() - 1;
+                continue;
+            }
+            res.append(base.charAt(j));
+        }
+        return res.toString();
+
+//        String[] baseArray = base.split(" ");
+//        StringBuilder res = new StringBuilder();
+//        for (int i = 0; i < baseArray.length; i++) {
+//            String word = baseArray[i];
+//            if (word.length() >= remove.length()) {
+//                StringBuilder replaced = new StringBuilder();
+//                for (int j = 0; j < word.length(); j++) {
+//                    if (remove.length() + j <= word.length() && word.substring(j, remove.length() + j).equalsIgnoreCase(remove)) {
+//                        j = j + remove.length() - 1;
+//                        continue;
+//                    }
+//                    replaced.append(word.charAt(j));
+//                }
+//                baseArray[i] = replaced.toString();
+//                if (baseArray[i].length() != 0) {
+//                    res.append(baseArray[i]).append(" ");
+//                }
+//            } else {
+//                res.append(baseArray[i]).append(" ");
+//            }
+//        }
+//        return res.toString();
     }
 
     public static int countYZ(String str) {
