bbc9193e8583fcaad3f4e3ae961c22f433235828
adilov2002 <joni.2002.08@gmail.com>
2022-09-05 13:09:32 +0600

    codingbat: finished String3 topic

--- src/codingbat/String3.java
+++ src/codingbat/String3.java
@@ -3,7 +3,150 @@ package codingbat;
 public class String3 {
 
     public static void main(String[] args) {
-        System.out.println(withoutString("Hellollo there", "llo"));
+
+    }
+
+    public static String notReplace(String str) {
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        if (str.equals("is")) {
+            return str + " not";
+        }
+        if (str.startsWith("is") && !Character.isLetter(str.charAt(2))) {
+            res.append("is not");
+            i = 2;
+        }
+
+        for (; i < str.length(); i++) {
+            if (i == str.length() - 3 && !Character.isLetter(str.charAt(i)) && str.substring(i + 1, i + 3).equals("is")) {
+                res.append(str.charAt(i));
+                res.append("is not");
+                break;
+            }
+            if (!Character.isLetter(str.charAt(i)) && !Character.isLetter(str.charAt(i + 3)) && str.substring(i + 1, i + 3).equals("is")) {
+                res.append(str.charAt(i));
+                res.append("is not");
+                i = i + 2;
+                continue;
+            }
+            res.append(str.charAt(i));
+        }
+        return res.toString();
+    }
+
+    public static int sumNumbers(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                for (int j = i; j < str.length(); j++) {
+                    if (i == str.length() - 1  && Character.isDigit(str.charAt(j))) {
+                        sum += Integer.parseInt(str.substring(i, j + 1));
+                        break;
+                    }
+                    if (i != str.length() - 1 && j == str.length() - 1 && Character.isDigit(str.charAt(j))) {
+                        sum += Integer.parseInt(str.substring(i, j + 1));
+                        i = j;
+                        break;
+                    }
+                    if (!Character.isDigit(str.charAt(j))) {
+                        sum += Integer.parseInt(str.substring(i, j));
+                        i = j;
+                        break;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static int maxBlock(String str) {
+        String largest = "";
+        boolean isBlock = false;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) != str.charAt(j)) {
+                    break;
+                }
+                if (str.charAt(i) == str.charAt(j) && str.substring(i, j + 1).length() > largest.length()) {
+                    isBlock = true;
+                    largest = str.substring(i, j + 1);
+                }
+            }
+        }
+        if (!isBlock && str.length() > 0) {
+            return 1;
+        }
+        return largest.length();
+    }
+
+    public static String mirrorEnds(String string) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0, j = string.length() - 1; i < string.length() && j >= 0; i++, j--) {
+            if (string.charAt(i) != string.charAt(j)) {
+                break;
+            }
+            res.append(string.charAt(i));
+        }
+        return res.toString();
+    }
+
+    public static String sameEnds(String string) {
+        String res = "";
+        for (int i = 1, j = string.length() - 1; i <= string.length() / 2 && j >= string.length() / 2; i++, j--) {
+            if (string.substring(0, i).equals(string.substring(j))) {
+                res = string.substring(0, i);
+            }
+        }
+        return res;
+    }
+
+    public static int sumDigits(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                sum += Integer.parseInt(str.charAt(i)+"");
+            }
+        }
+        return Math.max(sum, 0);
+    }
+
+    public static int countTriple(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            String part = str.substring(i, i + 3);
+            if (part.charAt(0) == part.charAt(1) && part.charAt(1) == part.charAt(2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean gHappy(String str) {
+        if (str.equals("g")) {
+            return false;
+        }
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (i == str.length() - 2 && str.charAt(i) != 'g' && str.charAt(i + 1) == 'g') {
+                return false;
+            }
+            if (str.charAt(i) == 'g' && str.charAt(i - 1) != 'g' && str.charAt(i + 1) != 'g') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean equalIsNot(String str) {
+        int countIs = 0;
+        int countNot = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ("is".length() + i <= str.length() && str.substring(i, "is".length() + i).equals("is")) {
+                countIs++;
+            } else if ("not".length() + i <= str.length() && str.substring(i, "not".length() + i).equals("not")) {
+                countNot++;
+            }
+        }
+        return countIs == countNot;
     }
 
     public static String withoutString(String base, String remove) {
